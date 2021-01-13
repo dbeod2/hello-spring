517dd819d19b21ee06e38b89367967a5064fedd4
dbeod2 <darung.0211@gmail.com>
2021-01-13 19:41:24 +0900

    spring web basic test

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import model.Hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +14,24 @@ public class HelloController {
   public String hello(Model model) {
     model.addAttribute("data", "hello!!");
     return "hello";
+  }
+
+  @GetMapping("hello-mvc")
+  public String helloMvc(@RequestParam(name = "name", required = false) String name, Model model) {
+    model.addAttribute("name", name);
+    return "hello-template";
+  }
+
+  @GetMapping("hello-string")
+  @ResponseBody
+  public String helloString(@RequestParam(name = "name", required = false) String name) {
+    return "hello " + name;
+  }
+
+  @GetMapping("hello-api")
+  @ResponseBody
+  public Hello helloApi(@RequestParam("name") String name) {
+    Hello hello = new Hello(name);
+    return hello;
   }
 }
---
+++ src/main/java/model/Hello.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Hello {
+  private String name;
+
+  public Hello(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+}
