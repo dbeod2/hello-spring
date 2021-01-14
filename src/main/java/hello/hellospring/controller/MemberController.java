c18a0a94621083297732bccf2ef6168a626e4ca0
dbeod2 <darung.0211@gmail.com>
2021-01-14 18:42:24 +0900

    회원 등록, 조회 구현

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,13 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
+import hello.hellospring.model.MemberForm;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +17,23 @@ public class MemberController {
   @Autowired
   public MemberController(MemberService memberService) {
     this.memberService = memberService;
+  }
+
+  @GetMapping("/members")
+  public String getAll(Model model) {
+    model.addAttribute("members", memberService.getAll());
+    return "members/memberList";
+  }
+
+  @GetMapping("/members/new")
+  public String createForm() {
+    return "members/createMemberForm";
+  }
+
+  @PostMapping("/members/new")
+  public String save(MemberForm memberForm) {
+    Member member = Member.of(memberForm.getName());
+    memberService.join(member);
+    return "redirect:/";
   }
 }
